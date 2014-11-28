04dfaf7ab1599f3abac0ff1194f67e1d161efe4d
Bianca <biancamsoe@gmail.com>
2014-11-28 17:56:01 -0200

    Added a new model in which the recovery and the infection cannot occur in the same step.

--- src/epidemics/separated_step_model.py
+++ src/epidemics/separated_step_model.py
@@ -27,7 +27,7 @@ class Epidemics(object):
 		self.infection_probability = (model_parameters if model == 'SI'
 				else model_parameters[0])
 		self.recovery_probability = None if model == 'SI' else model_parameters[1]
-		self.recovery_status = None if model == 'SI' else ('s' if model == 'SIS'
+		self.recovery_status = 0 if model == 'SI' else ('s' if model == 'SIS'
 				else 'r')
 		self.infected_nodes = [first_infected]
 		self.node_status = ['s' for _ in xrange(self.graph.vcount())]
@@ -39,28 +39,29 @@ class Epidemics(object):
 
 	def run_infection(self):
 		"""Runs the infection stage of the epidemic spread."""
-		newly_infected = []
-		for infected_node in self.infected_nodes:
-			neighbours = self.graph.neighbors(infected_node)
-			for neighbour in neighbours:
-				if (self.node_status[neighbour] == 's' and
-						self.random.uniform() < self.infection_probability):
-					self.node_status[neighbour] = 'i'
-					newly_infected.append(neighbour)
-		return list(set(newly_infected))
-
-	def run_recovery(self):
-		"""Runs the recovery stage of the epidemic spread."""
+		newly_infected, newly_recovered = [], []
 		for infected_node in self.infected_nodes:
 			if self.random.uniform() < self.recovery_probability:
-				self.node_status[infected_node] = self.recovery_status
-				self.infected_nodes.remove(infected_node)
+				newly_recovered.append(infected_node)
+			else:
+				neighbours = self.graph.neighbors(infected_node)
+				for neighbour in neighbours:
+					if (self.node_status[neighbour] == 's' and
+							self.random.uniform() < self.infection_probability):
+						self.node_status[neighbour] = 'i'
+						newly_infected.append(neighbour)
+		return newly_infected, newly_recovered
+
+	def run_recovery(self, nodes_to_recover):
+		"""Runs the recovery stage of the epidemic spread."""
+		for infected_node in nodes_to_recover:
+			self.node_status[infected_node] = self.recovery_status
+			self.infected_nodes.remove(infected_node)
 
 	def run_step(self):
 		"""Runs a step of the epidemic spread."""
-		newly_infected = self.run_infection()
-		if self.recovery_probability:
-			self.run_recovery()
+		newly_infected, newly_recovered = self.run_infection()
+		self.run_recovery(newly_recovered)
 		self.infected_nodes += newly_infected
 		for status in self.evolution:
 			self.evolution[status].append(self.node_status.count(status))
@@ -79,10 +80,11 @@ class Epidemics(object):
 		return False
 
 if __name__ == '__main__':
-	epi = Epidemics(igraph.GraphBase.Erdos_Renyi(100, 0.1), 'SIR', [0.3, 1],
+	epi = Epidemics(igraph.GraphBase.Erdos_Renyi(1000, 0.02), 'SIR', [0.6, 0.4],
 			0, 300)
 	epi.run()
 	size = len(epi.evolution['i'])
+	print epi.evolution['i']
 	x = range(0, size)
 	pyplot.plot(x, epi.evolution['s'])
 	pyplot.plot(x, epi.evolution['i'])
