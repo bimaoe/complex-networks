4fef2ea49340d635bdaae12c2057441f5d90a460
Bianca <biancamsoe@gmail.com>
2016-01-20 16:54:56 +0100

    Added runISR method that receives a reference for the status vector.

--- src/rumor/rumor.cpp
+++ src/rumor/rumor.cpp
@@ -7,7 +7,7 @@ void Rumor::Evolution::add(long long iCount, long long sCount, long long rCount)
   statusCount[STIFLER].push_back(rCount);
 }
 
-long long Rumor::Evolution::getSpreaderCount(void) {
+long long Rumor::Evolution::getReliability(void) {
   long long spreaderCount = statusCount[SPREADER].back();
   if (usedStatus == 3) {
     spreaderCount += statusCount[STIFLER].back();
@@ -46,10 +46,16 @@ void Rumor::initializeSpreader(vector<int> & initiallySpreader,
   }
 }
 
-Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu,
+Rumor::Evolution * Rumor::runISR(Graph & graph, double lambda, double alpha,
     vector<int> & initiallySpreader, int maxIterations) {
+  vector<Status> status;
+  return runISR(graph, lambda, alpha, initiallySpreader, maxIterations, status);
+}
+
+Rumor::Evolution * Rumor::runISR(Graph & graph, double lambda, double alpha,
+    vector<int> & initiallySpreader, int maxIterations, vector<Status> &status) {
   list<int> spreaderVertices;
-  vector<Status> status(graph.getVertexCount(), IGNORANT);
+  status = vector<Status>(graph.getVertexCount(), IGNORANT);
   Evolution * evolution = new Evolution();
   
   initializeSpreader(initiallySpreader, spreaderVertices, status);
@@ -61,9 +67,12 @@ Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu,
   evolution->add(iCount, sCount, rCount);
 
   for (int t = 0; spreaderVertices.size() > 0 &&  t < maxIterations; t++) {
-    fprintf (stderr, "(t, sz) = (%d %d)\n", t, spreaderVertices.size());
+    //fprintf (stderr, "(t, sz) = (%d %d)\n", t, spreaderVertices.size());
+    // Vector to store the nodes that turned into spreaders.
+    vector<int> newSpreaderVertices;
     // Vector to store the nodes that turned into stiflers.
     vector<decltype(spreaderVertices.begin())> newStiflers;
+
     // Iterate over spreader vertices.
     for (auto it = spreaderVertices.begin(); it != spreaderVertices.end(); it++) {
       int curr = *it;
@@ -81,17 +90,15 @@ Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu,
       for (auto it2 = shuffledNeighbours.begin(); it2 != shuffledNeighbours.end(); it2++) {
         int neighbour = *it2;
         if (status[neighbour] == IGNORANT) {
-          if (Stat::uniform() < delta) {
+          if (Stat::uniform() < lambda) {
             // Spreading.
-            status[neighbour] = SPREADER;
-            spreaderVertices.push_front(neighbour);
+            newSpreaderVertices.push_back(neighbour);
             iCount--;
             sCount++;
           }
         } else {
-          if (Stat::uniform() < nu) {
+          if (Stat::uniform() < alpha) {
             // Recovery.
-            status[curr] = STIFLER;
             newStiflers.push_back(it);
             sCount--;
             rCount++;
@@ -100,8 +107,14 @@ Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu,
         }
       }
     }
+    // Insert the new spreaders into the spreader list.
+    for (auto it = newSpreaderVertices.begin(); it != newSpreaderVertices.end(); it++) {
+      status[*it] = SPREADER;
+      spreaderVertices.push_back(*it);
+    }
     // Remove the new stiflers form the spreader list.
     for (auto it = newStiflers.begin(); it != newStiflers.end(); it++) {
+      status[*(*it)] = STIFLER;
       spreaderVertices.erase(*it);
     }
     evolution->add(iCount, sCount, rCount);
@@ -109,36 +122,36 @@ Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu,
   return evolution;
 }
 
-Rumor::Evolution * Rumor::runISR(Graph & graph, double delta, double nu, int firstInfected,
+Rumor::Evolution * Rumor::runISR(Graph & graph, double lambda, double alpha, int firstSpreader,
     int maxIterations) {
   vector<int> initiallySpreader;
-  initiallySpreader.push_back(firstInfected);
-  return runISR(graph, delta, nu, initiallySpreader, maxIterations);
+  initiallySpreader.push_back(firstSpreader);
+  return runISR(graph, lambda, alpha, initiallySpreader, maxIterations);
 }
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
 
-int main(void) {
-  Graph graph;
-  srand(time(NULL));
-  graph.readFromFile("../../../Data/macaco.edgelist");
-  Rumor::initialize();
-  Rumor::Evolution ans;
-  for (int it = 0; it < 30; it++) {
-    fprintf (stderr, "It: %d\n", it);
-    Rumor::Evolution * curr = Rumor::runISR(graph, 0.7, 0.3, 0, 30);
-    ans += curr;
-    delete curr;
-  }
-  for (int i = 0; i < ans.usedStatus; i++) {
-    printf ("%c = [", i?(i-1?'r':'s'):'i');
-    for (int j = 0; j < ans.statusCount[i].size(); j++) {
-      printf ("%s%lld", j?", ":"", ans.statusCount[i][j]);
-    }
-    printf ("]\n");
-  }
-} 
+// int main(void) {
+//   Graph graph;
+//   srand(time(NULL));
+//   graph.readFromFile("../../../Data/macaco.edgelist");
+//   Rumor::initialize();
+//   Rumor::Evolution ans;
+//   for (int it = 0; it < 30; it++) {
+//     fprintf (stderr, "It: %d\n", it);
+//     Rumor::Evolution * curr = Rumor::runISR(graph, 0.7, 0.3, 0, 30);
+//     ans += curr;
+//     delete curr;
+//   }
+//   for (int i = 0; i < ans.usedStatus; i++) {
+//     printf ("%c = [", i?(i-1?'r':'s'):'i');
+//     for (int j = 0; j < ans.statusCount[i].size(); j++) {
+//       printf ("%s%lld", j?", ":"", ans.statusCount[i][j]);
+//     }
+//     printf ("]\n");
+//   }
+// } 
--- src/rumor/rumor.h
+++ src/rumor/rumor.h
@@ -20,36 +20,48 @@ public:
     int usedStatus; // Should be 3 for IRS.
     vector<long long> statusCount[3]; // Should be indexed with the Status enum.
     void add(long long iCount, long long sCount, long long rCount);
-    long long getSpreaderCount(void); /* Get the number of spreader vertices.
+    long long getReliability(void); /* Get the number of spreader vertices.
                                         Includes stifler vertices. */
     void operator += (Evolution *e);
   };
 
   static void initialize(void);
 
-  /** Initialize the infected list and the status vector. **/
+  /** Initialize the spreader list and the status vector. **/
   static void initializeSpreader(vector<int> & initiallySpreader, list<int> & spreaderVertices,
       vector<Status> & status);
 
   /** Run the ISR Maki-Thompson model in a graph.
     Parameters:
       graph: The graph where the rumor will be spread.
-      delta: The infection probability, must be in [0, 1].
-      nu: The recovery probability, must be in [0, 1].
-      iniciallyInfected: The initially infected vertices' IDs.
+      lambda: The spreading probability, must be in [0, 1].
+      alpha: The recovery probability, must be in [0, 1].
+      iniciallySpreader: The initially spreader vertices' IDs.
+      maxIterations: Maximum number of iterations to run.
+      status: Vector of status (in case one wants to know the status of every node).
+  **/
+  static Rumor::Evolution * runISR(Graph & graph, double lambda, double alpha,
+    vector<int> & initiallySpreader, int maxIterations, vector<Status> &status);
+
+  /** Run the ISR Maki-Thompson model in a graph.
+    Parameters:
+      graph: The graph where the rumor will be spread.
+      lambda: The spreading probability, must be in [0, 1].
+      alpha: The recovery probability, must be in [0, 1].
+      iniciallySpreader: The initially spreader vertices' IDs.
       maxIterations: Maximum number of iterations to run.
   **/
-  static Rumor::Evolution * runISR(Graph & graph, double delta, double nu,
+  static Rumor::Evolution * runISR(Graph & graph, double lambda, double alpha,
       vector<int> & initiallySpreader, int maxIterations);
 
   /** Run the ISR Maki-Thompson model in a graph.
     Parameters:
       graph: The graph where the rumor will be spread.
-      delta: The infection probability, must be in [0, 1].
-      nu: The recovery probability, must be in [0, 1].
-      firstInfected: The initially infected vertex's ID.
+      lambda: The infection probability, must be in [0, 1].
+      alpha: The recovery probability, must be in [0, 1].
+      firstSpreader: The initially spreader vertex's ID.
       maxIterations: Maximum number of iterations to run.
   **/
-  static Rumor::Evolution * runISR(Graph & graph, double delta, double nu, int firstInfected,
+  static Rumor::Evolution * runISR(Graph & graph, double lambda, double alpha, int firstSpreader,
       int maxIterations);
 };
