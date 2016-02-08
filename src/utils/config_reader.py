6785387d6fec1c2cd858de0b5651c873a8690114
Bianca <biancamsoe@gmail.com>
2016-02-08 14:41:26 +0100

    Added functionality to insert lists and dictionaries inside lists.

--- src/utils/config_reader.py
+++ src/utils/config_reader.py
@@ -6,6 +6,8 @@ def convert(vartype, x):
     return int(x)
   elif vartype == 'float':
     return float(x)
+  elif vartype == 'bool':
+    return x in ['True', 'true']
 
 def parse(config_filename):
   config_file =open(config_filename, 'r')
@@ -25,17 +27,43 @@ def parse(config_filename):
       curr = next
     elif var[0] == '-':
       curr = stack.pop()
-    elif var[0] == 'single':
-      curr[var[1]] = var[2]
-    elif var[0] == 'list':
-      curr[var[1]] = var[2].split('|')
-    elif var[0] == 'dict':
-      curr[var[1]] = {}
-      next = curr[var[1]]
-    elif var[0] == 'tsingle':
-      curr[var[2]] = convert(var[1], var[3])
-    elif var[0] == 'tlist':
-      curr[var[2]] = [convert(var[1], _x) for _x in var[3].split('|')]
+
+    if isinstance(curr, dict):
+      print 'eh dict'
+      if var[0] == 'single':
+        curr[var[1]] = var[2]
+      elif var[0] == 'list':
+        if len(var) > 2:
+          curr[var[1]] = var[2].split('|')
+        else:
+          curr[var[1]] = []
+        next = curr[var[1]]
+      elif var[0] == 'dict':
+        curr[var[1]] = {}
+        next = curr[var[1]]
+      elif var[0] == 'tsingle':
+        curr[var[2]] = convert(var[1], var[3])
+      elif var[0] == 'tlist':
+        curr[var[2]] = [convert(var[1], _x) for _x in var[3].split('|')]
+        next = curr[var[2]]
+    elif isinstance(curr, list):
+      print 'eh list'
+      if var[0] == 'single':
+        curr.append(var[1])
+      elif var[0] == 'list':
+        if len(var) > 1:
+          curr.append(var[1].split('|'))
+        else:
+          curr.append([])
+        next = curr[-1]
+      elif var[0] == 'dict':
+        curr.append({})
+        next = curr[-1]
+      elif var[0] == 'tsingle':
+        curr.append(convert(var[1], var[2]))
+      elif var[0] == 'tlist':
+        curr.append([convert(var[1], _x) for _x in var[2].split('|')])
+        next = curr[-1]
   return config
 
 def main():
